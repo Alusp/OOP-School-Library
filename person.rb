46990dd360a6e5116ea842d47acc635478c99f00
Alusp <alusinejalloh800@gmail.com>
2022-11-03 16:13:23 +0200

    update

--- person.rb
+++ person.rb
@@ -1,6 +1,7 @@
-class Person
-  attr_accessor :name, :age
+require_relative './nameable'
 
+class Person < Nameable
+  attr_accessor :name, :age
   att_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -11,7 +12,11 @@ class Person
   end
 
   def can_use_services?
-    of_age? || @parent_permission
+    @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
