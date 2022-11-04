2ca932e8a5a3a13c9d979f35deff949d6806ed14
Alusp <alusinejalloh800@gmail.com>
2022-11-04 16:21:08 +0200

    update rentals file

--- rental.rb
+++ rental.rb
@@ -1,7 +1,10 @@
 class Rental
   attr_accessor :date, :person, :book
 
-  def initialize(date, person, book)
+  def initialize(date, book, person)
+    puts date
+    puts book
+    puts person
     @date = date
     @book = book
     book.rentals << self
